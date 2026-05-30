e76d2e48b45ad4d9f62f2fad10da0400df6488e3
Joao <joaovitorlopesdantas01@gmail.com>
2026-05-30 18:59:25 -0300

    Cleanup: removed redundant tasks_json and updated path logic

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def run():
             sys.exit()
         else:
             command = input("Incorrect usage. (ex.: :a, :l, :u, :d, :q)\n")
+            command_line()
 
     def home_screen():
         check_for_json()
@@ -38,27 +39,27 @@ def run():
         command_line()
 
     def check_for_json():
-        if os.path.isfile("/home/joao/Desktop/todo_app/tasks_json"):
+        if os.path.isfile(TODO_FILE):
             pass
         else:
             empty_array = []
-            with open("tasks_json", "w", encoding="utf-8") as json_file:
+            with open(TODO_FILE, "w", encoding="utf-8") as json_file:
                 json.dump(empty_array, json_file)
 
     def create_task():
-        with open("tasks_json", "r", encoding="utf-8") as json_file:
+        with open(TODO_FILE, "r", encoding="utf-8") as json_file:
             tasks_data = json.load(json_file)
         task_num = len(tasks_data)
         task_string = input("New task:\n")
         tasks_data.append({"id": (task_num + 1), "task": task_string})
-        with open("tasks_json", "w", encoding="utf-8") as json_file:
+        with open(TODO_FILE, "w", encoding="utf-8") as json_file:
             json.dump(tasks_data, json_file)
             print("New task added successfully!")
         command_line()
 
     def read_tasks():
         print("These are your current tasks:\n")
-        with open("tasks_json", "r", encoding="utf-8") as json_file:
+        with open(TODO_FILE, "r", encoding="utf-8") as json_file:
             tasks_data = json.load(json_file)
         task_num = len(tasks_data)
         for i in range(task_num):
@@ -74,11 +75,11 @@ def run():
         update_id = int(
             input("Select the task you want to update (ex: 1 for task No. 1, etc):\n")
         )
-        with open("tasks_json", "r", encoding="utf-8") as json_file:
+        with open(TODO_FILE, "r", encoding="utf-8") as json_file:
             tasks_data = json.load(json_file)
         updated_task = input("Updated Task:\n")
         tasks_data[(update_id - 1)] = {"id": update_id, "task": updated_task}
-        with open("tasks_json", "w", encoding="utf-8") as json_file:
+        with open(TODO_FILE, "w", encoding="utf-8") as json_file:
             json.dump(tasks_data, json_file)
             print("Task Updated successfully!")
         command_line()
@@ -88,7 +89,7 @@ def run():
         deleted_task = int(
             input("Select the task you want to delete (ex: 1 for task No. 1, etc):\n")
         )
-        with open("tasks_json", "r", encoding="utf-8") as json_file:
+        with open(TODO_FILE, "r", encoding="utf-8") as json_file:
             tasks_data = json.load(json_file)
         new_tasks_data = [
             {**d, "id": index}
@@ -96,11 +97,13 @@ def run():
                 (item for item in tasks_data if item.get("id") != deleted_task), start=1
             )
         ]
-        with open("tasks_json", "w", encoding="utf-8") as json_file:
+        with open(TODO_FILE, "w", encoding="utf-8") as json_file:
             json.dump(new_tasks_data, json_file)
             print("Task deleted successfully!")
         command_line()
 
     home_screen()
-    if __name__ == "__main__":
-        run()
+
+
+if __name__ == "__main__":
+    run()
